cf6558fd607a35251c9d51521b8efd90996b381e
tipx2 <tipx2gaming@gmail.com>
2022-08-24 17:44:23 +0100

    small bug fix

--- scenes/enemy_projectiles/enemy_projectile.gd
+++ scenes/enemy_projectiles/enemy_projectile.gd
@@ -9,20 +9,22 @@ var direction = Vector3.ZERO
 onready var death_timer = get_node("death_timer")
 onready var hitcast = get_node("hitcast")
 onready var player = get_tree().get_nodes_in_group("player")[0]
+onready var weak_player = weakref(get_tree().get_nodes_in_group("player")[0])
 
 
 func _ready():
 	death_timer.set_wait_time(expiry_time)
 
 func _physics_process(_delta):
-	hitcast.set_cast_to(global_transform.origin.direction_to(player.global_transform.origin) * hitbox_radius)
-	var collision_object = hitcast.get_collider()
-	
-	if collision_object and collision_object.is_in_group("player"):
-		collision_object.damage(damage)
-		queue_free()
-	
-	var _move = move_and_slide(direction * speed)
+	if weak_player.get_ref():
+		hitcast.set_cast_to(global_transform.origin.direction_to(player.global_transform.origin) * hitbox_radius)
+		var collision_object = hitcast.get_collider()
+		
+		if collision_object and collision_object.is_in_group("player"):
+			collision_object.damage(damage)
+			queue_free()
+		
+		var _move = move_and_slide(direction * speed)
 
 func set_target(target, muzzle):
 	direction = muzzle.global_transform.origin.direction_to(target.global_transform.origin)
--- scenes/pickups/health_pickup/health_pickup.gd
+++ scenes/pickups/health_pickup/health_pickup.gd
@@ -3,6 +3,6 @@ extends Area
 var amount;
 
 func _on_health_body_entered(body):
-	if body.name == "player" and body.player_health < 60:
+	if body.name == "player" and body.player_health < body.MAX_PLAYER_HEALTH:
 		body.damage(-amount)
 		queue_free()

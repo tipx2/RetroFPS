3cec2792494348ae0701871f8a0e075c7fba5198
tipx2 <57505742+tipx2@users.noreply.github.com>
2022-09-07 08:51:58 +0100

    created working master audio
    still working on music volume slider

--- scenes/UI/main_menu/options.gd
+++ scenes/UI/main_menu/options.gd
@@ -9,8 +9,13 @@ func _on_TextureButton_pressed():
 	set_visible(false)
 	Engine.set_target_fps($"%force_fps_box".value)
 	# set the true parent values to be saved
+	# video
 	true_parent.saved_fps = $"%force_fps_box".value
 	true_parent.saved_fullscreen = $"%fullscreen_button".pressed
+	
+	# audio
+	true_parent.saved_master_volume = $"%master_volume_slider".value
+	
 	# save the true_parent settings values to a file
 	save_settings(settings)
 	
@@ -19,7 +24,8 @@ func _ready():
 	# set the options buttons to saved values
 	$"%force_fps_box".value = true_parent.saved_fps
 	$"%fullscreen_button".pressed = true_parent.saved_fullscreen
-
+	$"%master_volume_slider".value = true_parent.saved_master_volume
+	#$"%music_volume_slider".value = true_parent.saved_music_volume
 
 func _on_fullscreen_button_toggled(button_pressed):
 	# toggle the fullscreen properties
@@ -28,10 +34,18 @@ func _on_fullscreen_button_toggled(button_pressed):
 	OS.window_maximized = !button_pressed
 	OS.window_resizable = !button_pressed
 
+func _on_master_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(value/100 * 2))
+
+func _on_music_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Soundtrack"), linear2db(value/100 * 2))
+
 func save_settings(settings_file):
 	var f = File.new()
 	f.open(settings_file, File.WRITE)
 	f.store_var(true_parent.saved_fps)
 	f.store_var(true_parent.saved_fullscreen)
+	f.store_var(true_parent.saved_master_volume)
+	f.store_var(true_parent.saved_music_volume)
 	# save all settings stored on true_parent here
 	f.close()
--- scenes/true_parent/true_parent.gd
+++ scenes/true_parent/true_parent.gd
@@ -2,8 +2,11 @@ extends Node
 
 var settings = "res://settings.save"
 
-var saved_fps; # these are defaults for the first time opening the game
+var saved_fps;
 var saved_fullscreen;
+
+var saved_master_volume;
+var saved_music_volume = 50
 
 var progression = [false,false, false, false, false] # etc 
 # one bool for each level
@@ -13,12 +16,18 @@ var main_menu = load("res://scenes/UI/main_menu/main_menu.tscn")
 
 func _ready():
 	load_settings(settings)
+	print(saved_master_volume)
 	# set variables to saved values
+	# video
 	Engine.set_target_fps(saved_fps)
 	OS.window_fullscreen = saved_fullscreen
 	OS.window_borderless = saved_fullscreen
 	OS.window_maximized = !saved_fullscreen
 	OS.window_resizable = !saved_fullscreen
+	
+	# audio
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(saved_master_volume/100 * 2))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Soundtrack"), linear2db(saved_master_volume/100 * 2))
 	
 	var menu_instance = main_menu.instance()
 	add_child(menu_instance)
@@ -33,5 +42,7 @@ func load_settings(settings_file):
 	f.open(settings_file, File.READ)
 	saved_fps = f.get_var()
 	saved_fullscreen = f.get_var()
+	saved_master_volume = f.get_var()
+	#saved_music_volume = f.get_var()
 	# put all saved settings here
 	f.close()

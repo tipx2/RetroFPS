6316491fb47b6678d20d2443e8f3dce189b67428
tipx2 <tipx2gaming@gmail.com>
2022-09-09 19:04:49 +0100

    changed explosion effect

--- scenes/weapon_projectiles/rocket/rocket.gd
+++ scenes/weapon_projectiles/rocket/rocket.gd
@@ -1,8 +1,10 @@
 extends "res://scenes/weapon_projectiles/projectile.gd"
 
 onready var explosion_area = get_node("explosion_area")
-onready var explosion_particles = get_node("explosion")
 onready var rocket_mesh = get_node("rocket_mesh")
+
+onready var explosive_mesh = get_node("explosive_mesh")
+onready var explosion_player = get_node("explosion_player")
 
 func _init():
 	rng.randomize()
@@ -13,8 +15,10 @@ func _init():
 
 func hit_enemies():
 	particles_emitted = true
-	explosion_particles.set_emitting(true)
+	explosive_mesh.set_visible(true)
+	explosive_mesh.rotation_degrees = Vector3(rand_range(0,360), rand_range(0,360), rand_range(0,360))
 	rocket_mesh.set_visible(false)
+	explosion_player.play("explode")
 	for body in explosion_area.get_overlapping_bodies():
 		if body.is_in_group("enemies") or body.is_in_group("player"):
 			var direction = body.global_transform.origin - global_transform.origin
@@ -27,13 +31,12 @@ func hit_enemies():
 				velocity.y *= 1.5
 				body.damage(5)
 			body.bonus_direction += velocity
-			
 
 func check_for_hit():
 	hit_enemies()
 
 func stop_emitting():
-	if particles_emitted and !explosion_particles.is_emitting():
+	if particles_emitted and !explosion_player.is_playing():
 		queue_free()
 
 func _on_death_timer_timeout():

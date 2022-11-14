e0c6149b1f64d80475879ffd890e724eac268ac3
tipx2 <57505742+tipx2@users.noreply.github.com>
2022-11-14 10:51:00 +0000

    added dropped platform

---
+++ scenes/level_parts/drop_trigger/drop_trigger.gd
@@ -0,0 +1,8 @@
+extends Spatial
+
+export(NodePath) onready var dropped_platform = get_node_or_null(dropped_platform)
+export(String) var drop_direction = "forward"
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("player"):
+		dropped_platform.drop(drop_direction)
